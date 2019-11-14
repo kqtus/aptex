a7f7cb227a70703558c8e9a35be87d3373c2b8aa
elMarco <elmarcopl@gmail.com>
2019-11-14 16:51:46 +0100

    added: Basket Summary.

---
+++ Aptex/ViewModels/BasketSummaryViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aptex.ViewModels
+{
+    public class BasketSummaryViewModel
+    {
+        public string CustomerFullName { get; set; }
+
+        public string CustomerEmail { get; set; }
+
+        public string AddrCity { get; set; }
+
+        public string AddrCode { get; set; }
+
+        public string AddrSt { get; set; }
+
+        public List<ProductViewModel> Products { get; set; }
+
+        public decimal TotalPrice { get; set; }
+    }
+}
