7376d49678d350bfd4981e8177ac24a97ce3181b
elMarco <elmarcopl@gmail.com>
2019-11-15 21:45:55 +0100

    added: Displaying items in basket.

--- Aptex.Web/Controllers/BasketController.cs
+++ Aptex.Web/Controllers/BasketController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Aptex.Contracts.Interfaces;
+using Aptex.Contracts.Models;
 using Aptex.Contracts.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,9 +11,41 @@ namespace Aptex.Web.Controllers
 {
     public class BasketController : Controller
     {
+        private readonly IProductsService productsService;
+
+        private readonly IBasketService basketService;
+
+        public BasketController(
+            IProductsService productsService,
+            IBasketService basketService)
+        {
+            this.productsService = productsService;
+            this.basketService = basketService;
+        }
+
         public IActionResult Index()
         {
-            return View("Basket");
+            var viewModel = new BasketSummaryViewModel()
+            {
+                Products = this.basketService
+                    .List()
+                    .Select(basketProduct =>
+                    {
+                        var product = productsService.Get(basketProduct.ProductId);
+
+                        return new ProductViewModel
+                        {
+                            ProductName = product.Name,
+                            ProductReception = product.Reception,
+                            Price = product.Price,
+                            Quantity = product.Quantity,
+                            BasketItemsCount = 1
+                        };
+                    })
+                    .ToList()
+            };
+            
+            return View("Basket", viewModel);
         }
 
         [HttpPost]
